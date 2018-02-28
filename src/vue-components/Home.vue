8086beb81d55b162409944e5bdb6afad63dad86f
administrator <liquidnuker@users.noreply.github.com>
2018-02-28 11:10:53 -0800

    live mode

--- src/vue-components/Home.vue
+++ src/vue-components/Home.vue
@@ -218,8 +218,8 @@ export default {
       vcSearch: vcSearch,
     },
     mounted: function () {
-      // this.getApiData(this.API_URL);
-      this.getApiData(this.BACKUP_URL);
+      this.getApiData(this.API_URL);
+      // this.getApiData(this.BACKUP_URL);
     },
     methods: {
       getApiData: function (url) {
